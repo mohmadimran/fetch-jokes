9e9db74959a62a496af11d6f780a0a2954a08ca0
mohmadimran <pathanimrankhan308@gmail.com>
2025-12-11 18:48:57 +0530

    add all file of fetch jokes app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState } from "react";
+import "./App.css"
 
 function App() {
+  const [jokes, setJokes] = useState(null);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = (false)
+
+  const fetchData = async () => {
+    setLoading(true)
+    try {
+      const res = await fetch("https://official-joke-api.appspot.com/random_joke")
+      const data = await res.json();
+      setJokes(data)
+    } catch (error) {
+      console.log(error)
+      setError(error)
+    }
+    finally { setLoading(false) }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <h1 className="title">Random Joke</h1>
+      <p className="subtitle">Click the button to fetch a fresh one</p>
+      <button className="btn" onClick={fetchData}>{loading ? " Fetching…" : "Fetch joke"}</button>
+      <div>{jokes && <p>{jokes.punchline}</p>}</div>
+      <div>{error && <p className="joke">No joke yet</p>}</div>
     </div>
   );
 }
